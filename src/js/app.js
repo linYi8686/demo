9b523f95a32e81b4500db39c4dd1b7f0dd9de67b
linY8686 <liny8686@github.com>
2018-03-31 16:55:05 +0800

    1.0.0

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+class app extends React.Component{
+	render(){
+		return <div id='div1'>
+                    react;
+		      </div>
+	}
+}
+export  default app;
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './../css/style'
+import  App from  './app'
+ReactDOM.render(
+     <App />,
+     document.getElementById("div1")
+	)
